47481a7335db5fb5062eb743097b8350580bb03e
Alka Pandit <alkapandit6101998@gmail.com>
2025-10-30 10:31:09 +0530

    feat: database connection done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+import dotenv from "dotenv";
 import express from "express";
 import contactsRouter from "./routes/contacts.route.js";
 import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+import { connectDB } from "./db/dbConnection.js";
+
+dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
